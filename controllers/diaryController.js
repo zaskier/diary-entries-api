6c2a8da1fdb6cca4fa5c0d182b0bfeb28710d442
iskierkw <wojciech.iskierka@contractors.roche.com>
2021-01-05 20:10:13 +0100

    init diary Controller

---
+++ controllers/diaryController.js
@@ -0,0 +1,27 @@
+function diaryController(Diary){
+    function post(req, res) {
+        const diary = new Diary(req.body);
+        console.log('record was added');
+        console.log(diary);
+    
+        diary.save();
+        return res.status(201).json(diary);
+    }
+    function get(req, res) {
+        const query = {};
+        if (req.query._id) {
+            query._id = req.query._id;
+        }
+        Diary.find(query, (err, diary) =>{
+        if(err){
+            return res.send(err);  
+          } else {
+             return res.json(diary) 
+          }
+        })
+     
+    }
+    return { post, get }
+}
+
+module.exports = diaryController;
