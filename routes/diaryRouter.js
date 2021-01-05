ea8b679aefda6f8554832cc091c62e7d5a37cca8
iskierkw <wojciech.iskierka@contractors.roche.com>
2021-01-05 19:33:43 +0100

    added PATCH, DELETE code cleanup with middleware

--- routes/diaryRouter.js
+++ routes/diaryRouter.js
@@ -26,31 +26,62 @@ function routes(Diary){
         return res.status(201).json(diary);
       });
 
-   diaryRouter.route('/diary/:entryID')
-   .get((req, res) => {
-     Diary.findById(req.params.entryID, (err, diary) => {
-       if (err) {
-         return res.send(err);
-       }
-       return res.json(diary);
-     });
-   })
-   .put((req, res) => {
-     Diary.findById(req.params.entryID, (err, diary) => {
+   diaryRouter.use('/diary/:entryID', (req, res, next) =>{
+    Diary.findById(req.params.entryID, (err, diary) => {
         if (err) {
           return res.send(err);
         }
-        diary.userID = req.body.userID;
-        diary.title = req.body.title;
-        diary.Content = req.body.Content; //TODO replace with small letter 
-        diary.weather = req.body.weather;
-        diary.save();
-        return res.json(diary);
+        if(diary){
+            req.diary = diary;
+            return next();
+        }
+        return res.sendStatus(404);
       });
+   })
+   diaryRouter.route('/diary/:entryID')
+    .get((req, res) => res.json(req.diary))
+    .put((req, res) => {
+            const {diary} = req;
+            diary.userID = req.body.userID;
+            diary.title = req.body.title;
+            diary.Content = req.body.Content; //TODO replace with small letter 
+            diary.weather = req.body.weather;
+            req.diary.save((err) => {
+                if (err) {
+                    return res.json(diary);
+                }
+                return res.json(diary);
+                });
+     })
+    .patch((req, res) =>{
+         const {diary} = req;
+        // ${typeof req.body.userID === "undefined"  ? `` : `${req.body.userID}",`}
+        if (req.body._id){
+            delete req.body._id;
+        }
+        Object.entries(req.body).forEach((item) =>{
+            const key = item[0];
+            const value = item[1];            
+            diary [key] = value;
+    })
+    req.diary.save((err) => {
+        if (err) {
+            return res.json(diary);
+        }
+        return res.json(diary);
+        });
+    })
+    .delete((req, res) =>{
+        req.diary.remove((err)=> {
+            if (err) {
+                return res.send(err);
+            }
+            console.log(`Entry ' ${req.params.entryID} ' was deleted`);
+            return res.sendStatus(204);
+        });  
+
     });
-
-
-      return diaryRouter
+    return diaryRouter
 }
 
 module.exports = routes;
