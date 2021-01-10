d12acd0e18a9d798c27b3976888acd051992111c
iskierkw <wojciech.iskierka@contractors.roche.com>
2021-01-10 22:08:26 +0100

    modified for test's, added 2 version of Databases

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app= express( );
-const db = mongoose.connect('mongodb://localhost/diary');
+if (process.env.ENV === 'Test') {
+  console.log('This is a test');
+  const db = mongoose.connect('mongodb://localhost/diary_Test');
+} else {
+  console.log('This is for real');
+  const db = mongoose.connect('mongodb://localhost/diary-Prod');
+}
+//const db = mongoose.connect('mongodb://localhost/diary');
 // eslint-disable-next-line no-undef
 const port = process.env.PORT || 8080;
 const Diary = require('./models/diaryModel');
@@ -11,12 +18,9 @@ const Diary = require('./models/diaryModel');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//const diaryRouter = express.Router(); TOBEREMOVED
 const diaryRouter =require('./routes/diaryRouter')(Diary);
 
-
 app.use('/api',diaryRouter);
-
 
 app.get('/', (req, res)=>{
     res.send('Diary API');
@@ -25,3 +29,5 @@ app.get('/', (req, res)=>{
 app.listen(port, ()=>{
     console.log(`Running on Port : ${port}` );
     });
+
+    module.exports = app;
