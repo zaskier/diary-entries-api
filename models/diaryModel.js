7949b2341101afa1e824a7759642c53e9e577474
iskierkw <wojciech.iskierka@contractors.roche.com>
2021-01-10 22:15:11 +0100

    new data model

--- models/diaryModel.js
+++ models/diaryModel.js
@@ -6,8 +6,19 @@ const diaryModel = new Schema(
   {
     userID: { type: String },
     title: { type: String },
-    Content: { type: String },
-    weather: { type: String },
+    content: { type: String },
+    weather: {
+      shortDescription: { type: String },
+      temperature: { type: String },
+      humidity: { type: String },
+      pressure:{ type: String },
+      visibility:{ type: String },
+      windSpeed:{ type: String },
+      windDeg:{ type: String },
+      clouds:{ type: String },
+      matchedLocatioName:{ type: String },
+      },
+    location: { type: String },
   }
 );
 
