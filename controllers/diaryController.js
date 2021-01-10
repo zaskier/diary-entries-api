c1b2c8f29af2b0022952f979c1c5e9d0b8ae534e
iskierkw <wojciech.iskierka@contractors.roche.com>
2021-01-10 22:06:44 +0100

    added weather API, added required parameters for request,added error handling forrequest without location

--- controllers/diaryController.js
+++ controllers/diaryController.js
@@ -1,17 +1,58 @@
+const fetch = require('node-fetch');
+const config = require('./weather-api-config.json');
 function diaryController(Diary){
-    function post(req, res) {
-        const diary = new Diary(req.body);
-        console.log('record was added');
-        console.log(diary);
-    
-        diary.save();
-        res.status(201);
-        return res.json(diary);
+function post(req, res) {
+ const diary = new Diary(req.body);
+ async function postEntry(){
+    let status;
+    const weatherApiResponse =  await fetch(`${config.weatherURL}?q=${diary.location}&appid=${config.appID}`)
+    .then((res) => { 
+        status = res.status; 
+        return res.json() 
+      })
+    console.log('record was added');
+    console.log(status);
+      if (status == 404) { 
+        console.log("Error : there was an isssue with weather API, validate 'location' parameter value or APIU is not working");
+        diary.weather.shortDescription="";
+        diary.weather.temperature = "";
+        diary.weather.humidity = "";
+        diary.weather.pressure = "";
+        diary.weather.visibility = "";
+        diary.weather.windDeg = "";
+        diary.weather.windSpeed ="";
+        diary.weather.clouds ="";
+        diary.weather.matchedLocatioName="";
+        }
+     else{
+        diary.weather.shortDescription=weatherApiResponse.weather[0].description;
+        diary.weather.temperature = weatherApiResponse.main.temp;
+        diary.weather.humidity = weatherApiResponse.main.humidity
+        diary.weather.pressure = weatherApiResponse.main.pressure;
+        diary.weather.visibility = weatherApiResponse.visibility;
+        diary.weather.windDeg = weatherApiResponse.wind.speed;
+        diary.weather.windSpeed =weatherApiResponse.wind.deg;
+        diary.weather.clouds =weatherApiResponse.clouds.all;
+        diary.weather.matchedLocatioName =weatherApiResponse.name;     
+       }
+       console.log(diary);
+       diary.save();
+       if (!req.body.title || !req.body.content || !req.body.userID) {
+           res.status(400);
+           return res.send('Title, content and UserID are required');
+         }
+       res.status(201);
+       return res.json(diary);
+
+}
+postEntry();
+     
     }
+
     function get(req, res) {
         const query = {};
-        if (req.query._id) {
-            query._id = req.query._id;
+        if (req.query.userID) {
+            query.userID = req.query.userID;
         }
         Diary.find(query, (err, diary) =>{
         if(err){
@@ -26,3 +67,5 @@ function diaryController(Diary){
 }
 
 module.exports = diaryController;
+
+
